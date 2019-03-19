84fc1a650e5f38b5920bd8ae5d5f3d8a93dac919
Noah304555 <47399979+Noah304555@users.noreply.github.com>
2019-03-19 09:24:45 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _304555fileReadExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+                System.IO.StreamReader sr =
+                    new System.IO.StreamReader("birthdayList.txt");
+                while(!sr.EndOfStream)
+                {
+                    lblOutput.Content += sr.ReadLine() + Environment.NewLine; 
+                }//end while
+                sr.Close();
+            }//end try
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }//end catch
+
+        }
+    }
+}
